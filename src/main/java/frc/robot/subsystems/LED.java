1d11e475a5172dc116562a8a980916e79e8a0eb3
Cephandrius42 <94200651+Cephandrius42@users.noreply.github.com>
2024-01-13 13:24:33 -0500

    Add LED library

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -6,15 +6,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.LedStrip;
+import frc.PredefinedColors;
+import frc.Animations.RaceAnimation;
+import frc.Animations.BlinkAnimation;
 
-public class Drivetrain extends SubsystemBase {
-	/** Creates a new Drivetrain. */
-	public Drivetrain() {
-		
-	}
+public class LED extends SubsystemBase {
+  /** Creates a new LED. */
+  public LED() {}
 
-	@Override
-	public void periodic() {
-		// This method will be called once per scheduler run
-	}
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
