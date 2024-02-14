048d45124dfb073b5539b0661d1391c21f448e27
SamohAwesome <72154565+SamohAwesome@users.noreply.github.com>
2024-02-14 15:24:47 -0500

    Refactor RobotContainer and Drivetrain classes
    - Remove extra blank lines in RobotContainer class
    - Add comments for PathPlanner stuff in RobotContainer constructor
    - Comment out NamedCommands.registerCommand() call in RobotContainer constructor
    - Remove getAutonomousCommand() method from Drivetrain class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -48,6 +48,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
 	// The robot's subsystems and commands are defined here...
 	private final ShuffleboardInfo shuffleboard;
+	
+	
 
 	// Replace with CommandPS4Controller or CommandJoystick if needed
 	private final CommandXboxController driverController = new CommandXboxController(
@@ -67,6 +69,8 @@ public class RobotContainer {
 	 */
 	public RobotContainer() {
 		//PathPlanner stuff
+		//Naming Commands has to be done before drivetrain is created
+		// NamedCommands.registerCommand("autoBalance", swerve.autoBalanceCommand());
 		// Subsystem initialization
 
 		// Register Named Commands
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -134,19 +134,6 @@ public class Drivetrain extends SubsystemBase
    * @param setOdomToStart Set the odometry position to the start of the path.
    * @return {@link AutoBuilder#followPath(PathPlannerPath)} path command.
    */
-  public Command getAutonomousCommand(String pathName, boolean setOdomToStart)
-  {
-    // Load the path you want to follow using its name in the GUI
-    PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
-
-    if (setOdomToStart)
-    {
-      resetOdometry(new Pose2d(path.getPoint(0).position, getHeading()));
-    }
-	
-    // Create a path following command using AutoBuilder. This will also trigger event markers.
-    return AutoBuilder.followPath(path);
-  }
 
   /**
    * Command to drive the robot using translative values and heading as a setpoint.
