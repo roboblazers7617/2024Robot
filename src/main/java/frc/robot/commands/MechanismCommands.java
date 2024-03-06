2b33fa37d2bfc2e22d79c219c4585af8ae256b59
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2024-03-06 18:10:56 -0500

    get distance for shooting from drivetrain

--- src/main/java/frc/robot/commands/MechanismCommands.java
+++ src/main/java/frc/robot/commands/MechanismCommands.java
@@ -4,19 +4,20 @@ import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Head;
 
 public class MechanismCommands {
-	public Command ShootAmp(Arm arm, Head head, double distance) {
+	public Command ShootAmp(Arm arm, Head head) {
 		return new InstantCommand(() -> arm.setArmTarget(ArmConstants.MAX_ANGLE)).andThen(new InstantCommand(() -> arm.setElevatorTarget(ElevatorConstants.MAX_HEIGHT))).andThen(head.ShootInAmp());
 	}
 	
-	public Command ShootSpeaker(Arm arm, Head head, double distance) {
+	public Command ShootSpeaker(Arm arm, Head head, Drivetrain drivetrain) {
+		double distance = drivetrain.getDistanceToSpeaker();
 		InterpolatingDoubleTreeMap map = new InterpolatingDoubleTreeMap();
 		map.put(0.0, 0.0);
 		return Commands.runOnce(() -> arm.setArmTarget(map.get(distance))).andThen(head.ShootAtPosition(distance));
