8f7ca9a15e52b3a04828c7618e8c820dc0d8b133
Max Nargang <CoffeeCoder1@outlook.com>
2024-03-12 18:35:32 -0400

    Add driver camera

--- src/main/java/frc/robot/shuffleboard/DriverStationTab.java
+++ src/main/java/frc/robot/shuffleboard/DriverStationTab.java
@@ -4,6 +4,8 @@
 
 package frc.robot.shuffleboard;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.IntegerPublisher;
@@ -26,6 +28,8 @@ public class DriverStationTab extends ShuffleboardTabBase {
 	private final BooleanPublisher isBrownedOut;
 	private Alert alert = new Alert("something is wrong", AlertType.ERROR);
 	
+	private UsbCamera camera;
+	
 	public DriverStationTab() {
 		NetworkTableInstance inst = NetworkTableInstance.getDefault();
 		
@@ -35,6 +39,12 @@ public class DriverStationTab extends ShuffleboardTabBase {
 		voltagePublisher = networkTable.getDoubleTopic("Battery Voltage").publish();
 		isDriverStationConnected = networkTable.getBooleanTopic("Is Driver Station Connected").publish();
 		isBrownedOut = networkTable.getBooleanTopic("Is Browned Out").publish();
+		
+		camera = CameraServer.startAutomaticCapture();
+		if (camera.isConnected()) {
+			camera.setResolution(480, 320);
+			camera.setFPS(10);
+		}
 	}
 	
 	@Override
