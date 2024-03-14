54b1c61524c9945df803baa65d7f3a78d024b23f
Max Nargang <CoffeeCoder1@outlook.com>
2024-03-14 12:06:34 -0400

    Seperate driver and operator joystick deadbands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -129,7 +129,8 @@ public final class Constants {
 	}
 
 	public static class OperatorConstants {
-		public static final double JOYSTICK_DEADBAND = 0.075;
+		public static final double DRIVER_JOYSTICK_DEADBAND = 0.075;
+		public static final double OPERATOR_JOYSTICK_DEADBAND = 0.125;
 		public static final int DRIVER_CONTROLLER_PORT = 0;
 		public static final int OPERATOR_CONTROLLER_PORT = 1;
 		public static final double ROTATION_RATE = 2;
